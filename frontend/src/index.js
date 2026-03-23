31d8ec7a3faad77966d0c4ef624dad5230467187
Karina <raditoiukarina@gmail.com>
2026-03-23 22:33:29 +0200

    deploy steps

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./styles.css";
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
